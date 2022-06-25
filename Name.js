256bf2a786093d48e99acc47688f84a7d5add229
Mrwise01 <105334289+Mrwise01@users.noreply.github.com>
2022-06-25 16:20:48 +0100

    this is just a printing demo

---
+++ Name.js
@@ -0,0 +1,7 @@
+
+// console.log("Name: Abdullahi Abdulzeez");
+// console.log("Height: 6.7");
+// console.log("Country: Nigeria");
+function MyName() { }
+MyName.displayName = "Abdullahi"
+console.log(MyName.displayName)
